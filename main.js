7a38953f785494e61763765a3b84a02c7aa8391b
fabysoouza <fabysouza0525@gmail.com>
2026-07-20 10:51:14 -0300

    feat: adiciona getbyid no javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form-contato');
+
+const nomes = [];
+const numeros = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    adicionaContato();
+})
+
+function adicionaContato(){
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    
+
+    
+}
